ea94755a70bb09687420284db186124443272142
Leonardo Moreno <leonardomoreno@alunos.utfpr.edu.br>
2022-10-23 03:33:54 -0300

    Removed unused imports in Header Component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import styles from "./styles.module.scss";
-import Link from 'next/link'
-import ShadowButton from '../ShadowButton';
-import { Formik, Form, Field, ErrorMessage } from "formik";
-import * as yup from "yup"
-import Axios from "axios"
-import { ToastContainer } from 'react-toastify'
-import ToastError from "../../components/ToastError/index"
 import Router from 'next/router';
 import jsCookie from 'js-cookie';
 
